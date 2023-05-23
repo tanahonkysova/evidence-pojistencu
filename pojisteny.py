e9ca74c12072c8f98d2310ae7509287884396dd1
tanahonkysova <134221838+tanahonkysova@users.noreply.github.com>
2023-05-23 15:05:58 +0200

    Create pojisteny.py

---
+++ pojisteny.py
@@ -0,0 +1,10 @@
+class Pojisteny:
+
+    def __init__(self, jmeno, prijmeni, telefonni_cislo, vek):
+        self.jmeno = jmeno
+        self.prijmeni = prijmeni
+        self.telefonni_cislo = telefonni_cislo
+        self.vek = vek
+
+    def __str__(self):
+        return f"{self.jmeno} | {self.prijmeni} | {self.vek} | {self.telefonni_cislo}"
